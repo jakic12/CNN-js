328e62b06fd26ccef86f575e49aa455cd0707e19
jakob <drusany.jakob@gmail.com>
2019-07-28 18:51:55 +0200

    Added shape confirmation and lenet5

--- cnn.js
+++ cnn.js
@@ -1,35 +1,86 @@
 class CNN{
-    constructor(){
+    constructor(shape){
+        CNN.confirmShape(shape)
+        
+        this.shape = shape
+    }
 
+    static confirmShape(shape){
+        if(shape[0].type != LayerType.INPUT)
+            throw new Error(`the first layer isn't an input layer, instead is: ${shape[0].type}`)
+        for(let i = 1; i < shape.length; i++){
+            if(shape[i].type == LayerType.CONV){
+                if(shape[i].w != (shape[i-1].w - shape[i].f + 2 * shape[i].p) / shape[i].s + 1)
+                    throw new Error(`[${i}] CONV: outW doesn't equal to calculated outW expected: ${(shape[i-1].w - shape[i].f + 2 * shape[i].p) / shape[i].s + 1}, actual: ${shape[i].w}`)
+
+                if(shape[i].h != (shape[i-1].h - shape[i].f + 2 * shape[i].p) / shape[i].s + 1)
+                    throw new Error(`[${i}] CONV: outH doesn't equal to calculated outH expected: ${(shape[i-1].h - shape[i].f + 2 * shape[i].p) / shape[i].s + 1}, actual: ${shape[i].h}`)
+
+                if(shape[i].d != shape[i].k)
+                    throw new Error(`[${i}] CONV: number of kernels doesn't equal outD kernels: ${shape[i].k}, outD: ${shape[i].d}`)
+
+            }else if(shape[i].type == LayerType.POOL){
+                if(shape[i].w != (shape[i-1].w - shape[i].f) / shape[i].s + 1)
+                    throw new Error(`[${i}] POOL: outW doesn't equal to calculated outW expected: ${(shape[i-1].w - shape[i].f) / shape[i].s + 1}, actual: ${shape[i].w}`)
+                
+                if(shape[i].h != (shape[i-1].h - shape[i].f) / shape[i].s + 1)
+                    throw new Error(`[${i}] POOL: outH doesn't equal to calculated outH expected: ${(shape[i-1].h - shape[i].f) / shape[i].s + 1}, actual: ${shape[i].h}`) 
+                
+                if(shape[i-1].d != shape[i].d)
+                throw new Error(`[${i}] POOL: outD doesn't equal inZ inZ: ${shape[i-1].d}, outD: ${shape[i].d}`)
+            }else if(shape[i].type == LayerType.FC){
+
+            }
+        }
+
+        return true
     }
 }
 
-let LayerType = {
+const LayerType = {
     CONV: 0,
     ACTIV: 1,
-    POOL: 2
+    POOL: 2,
+    FLATTEN: 3,
+    FC:4,
+    INPUT:5
 }
 
-let sigm = x => 1/(1+Math.exp(-x))
-let ActivationFunction = {
+const sigm = x => 1/(1+Math.exp(-x))
+const ActivationFunction = {
     RELU: x => x>0? x : 0,
     DRELU: x => x>0? 1 : 0,
     SIGMOID: sigm,
-    DSIGMOID: x => sigm(x)*(1 - sigm(x))
+    DSIGMOID: x => sigm(x)*(1 - sigm(x)),
+    TANH: Math.tanh,
+    DTANH: x => (1 - Math.pow(Math.tanh(x), 2))
 }
 
-let Layer = {
+const Layer = {
     /**
-     * Convolution layer hyperparameters
+     * The input layer of a network
      * @param {Number} w width of the input
      * @param {Number} h height of the input
      * @param {Number} d depth of the input
+     */
+    INPUT: function(w, h, d){
+        this.type = LayerType.INPUT
+        this.w = w
+        this.h = h
+        this.d = d
+    },
+    /**
+     * Convolution layer hyperparameters
+     * @param {Number} w width of the output
+     * @param {Number} h height of the output
+     * @param {Number} d depth of the output
      * @param {Number} f filter size (x,y)
      * @param {Number} k number of filters
      * @param {Number} s stride
      * @param {Number} p zero padding
+     * @param {Number} af activation function
      */
-    CONV: function(w, h, d, f, k, s, p){
+    CONV: function(w, h, d, f, k, s, p, af){
         this.type = LayerType.CONV
         this.w = w
         this.h = h
@@ -38,28 +89,54 @@ let Layer = {
         this.k = k
         this.s = s
         this.p = p
-    },
-    /**
-     * Activation layer
-     * @param {Function} f the activation function
-     */
-    ACTIV: function(f){
-        this.type = LayerType.ACTIV
-        this.f = f
+        this.af = af
     },
     /**
      * Pooling layer hyperparameters
-     * @param {Number} w width of the input
-     * @param {Number} h height of the input
-     * @param {Number} d depth of the input
+     * @param {Number} w width of the output
+     * @param {Number} h height of the output
+     * @param {Number} d depth of the output
      * @param {Number} f filter size
      * @param {Number} s stride
+     * @param {Number} af activation function
      */
-    POOL: function(w, h, d, f, s){
-
+    POOL: function(w, h, d, f, s, af){
+        this.type = LayerType.POOL
+        this.w = w
+        this.h = h
+        this.d = d
+        this.f = f
+        this.s = s
+        this.af = af
+    },
+    /**
+     * A fully connected layer
+     * @param {Number} l length of the layer
+     * @param {Number} af activation function
+     */
+    FC: function(l, af){
+        this.type = LayerType.FC
+        this.l = l
+        this.af = af
     }
 }
 
+const NetworkArchitectures = {
+    LeNet5: [
+        new Layer.INPUT(32, 32, 1),
+        new Layer.CONV(28, 28, 6, 5, 6, 1, 0, ActivationFunction.TANH),
+        new Layer.POOL(14, 14, 6, 2, 2, ActivationFunction.TANH),
+        new Layer.CONV(10, 10, 16, 5, 16, 1, 0, ActivationFunction.TANH),
+        new Layer.POOL(5, 5, 16, 2, 2, ActivationFunction.TANH),
+        new Layer.CONV(1, 1, 120, 5, 120, 1, 0, ActivationFunction.TANH),
+        new Layer.FC(84, ActivationFunction.TANH),
+        new Layer.FC(10, ActivationFunction.TANH)
+    ]
+}
+
 module.exports = {
-    CNN
+    CNN,
+    ActivationFunction,
+    Layer,
+    NetworkArchitectures
 }
--- tests/cnn.js
+++ tests/cnn.js
@@ -1,6 +1,31 @@
 const { expect } = require(`chai`)
-const { CNN } = require(`../cnn`)
+const {
+    CNN,
+    Layer,
+    ActivationFunction,
+    NetworkArchitectures
+} = require(`../cnn`)
 
 describe('Convolutional neural network', () => {
+    it(`LeNet5 doesn't throw error`, () => {
+        expect(() => new CNN(NetworkArchitectures.LeNet5)).not.to.throw()
+    })
 
+    it(`confirmShape`, () => {
+        expect(() => {
+            new CNN([ new Layer.INPUT(12, 12, 2), new Layer.CONV(5, 5, 3, 10, 3, 2, 3) ])
+        }).not.to.throw()
+
+        expect(() => {
+            new CNN([ new Layer.INPUT(10, 10, 3), new Layer.POOL(3, 3, 3, 4, 3) ])
+        }).not.to.throw()
+
+        expect(() => {
+            new CNN([ new Layer.INPUT(10, 10, 3), new Layer.POOL(3, 3, 3, 3, 3) ])
+        }).to.throw()
+
+        expect(() => {
+            new CNN([ new Layer.INPUT(12, 12, 2), new Layer.CONV(5, 5, 3, 10, 3, 2, 1) ])
+        }).to.throw()
+    })
 })
