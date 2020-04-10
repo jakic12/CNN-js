7f9cf6e8180474a7a1a77603d1da981b69f2ba60
jakob <drusany.jakob@gmail.com>
2020-04-10 19:39:57 +0200

    Added data normalization

--- test/imageProcessor.js
+++ test/imageProcessor.js
@@ -1,13 +1,23 @@
 const { imageToArray, arrayToImage } = require(`../imageProcessor`);
+const { deepNormalize, deepMap } = require(`../math`);
 
 describe("Image processor tests", () => {
   it(`Image processor test converts to array`, async () => {
-    console.log(await imageToArray(`test/testImage.jpg`, { x: 100, y: 100 }));
+    console.log(
+      deepNormalize(
+        await imageToArray(`test/testImage.jpg`, { x: 100, y: 100 })
+      )
+    );
   });
 
   it(`Image processor correctly does the 2 way transformation`, async () => {
     await arrayToImage(
-      await imageToArray(`test/testImage.jpg`, { x: 150, y: 100 }),
+      deepMap(
+        deepNormalize(
+          await imageToArray(`test/testImage.jpg`, { x: 100, y: 100 })
+        ),
+        (e) => e * 255
+      ),
       `test/output.jpg`
     );
   });
