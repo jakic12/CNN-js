f3fceebe51cbcb03b1fe148f57d62bbbdfbf52b9
jakob <drusany.jakob@gmail.com>
2020-05-05 16:06:43 +0200

    added confusion matrix to web_worker

--- training_web_worker.js
+++ training_web_worker.js
@@ -1,19 +1,27 @@
 const {CNN} = require("./cnn");
 
 self.addEventListener("message", message => {
-  const neuralNet = new CNN(JSON.parse(message.data.network));
-  const eventFnct = (eventName, d) => {
-    self.postMessage({
-      event: eventName,
-      data: d,
-      network: JSON.stringify(neuralNet),
-    });
-  };
+  if (!message.data.cmd) {
+    const neuralNet = new CNN(JSON.parse(message.data.network));
+    const eventFnct = (eventName, d) => {
+      self.postMessage({
+        event: eventName,
+        data: d,
+        network: JSON.stringify(neuralNet),
+      });
+    };
 
-  neuralNet.sgd(
-    Object.assign(message.data.trainingProps, {
-      onProgress: (...d) => eventFnct("batchProgress", d),
-      onEnd: (...d) => eventFnct("end", d),
-    }),
-  );
+    neuralNet.sgd(
+      Object.assign(message.data.trainingProps, {
+        onProgress: (...d) => eventFnct("batchProgress", d),
+        onEnd: (...d) => eventFnct("end", d),
+      }),
+    );
+  } else {
+    if (message.data.cmd === `confusionMatrix`) {
+      const neuralNet = new CNN(JSON.parse(message.data.network));
+      const cm = neuralNet.confusionMatrix(message.data.dataset);
+      self.postMessage({event: "confusionMatrix", data: cm});
+    }
+  }
 });
